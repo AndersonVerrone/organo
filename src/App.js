f09e330bdd57031d07dc46b81a553674aabe0717
Anderson Verrone <doverrone@gmail.com>
2022-10-22 16:00:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Banner from './Componentes/Banner/Banner.js';
+import ListaSuspensa from './Componentes/ListaSuspensa/index.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <ListaSuspensa />
+    </div>
+  );
+}
+
+export default App;
